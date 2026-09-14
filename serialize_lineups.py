0cbdc966c02a70dac69e79eaac319494007dc56f
Hi237271 <avigupta761@gmail.com>
2026-09-14 21:38:48 +0000

    Added lineups.json

---
+++ serialize_lineups.py
@@ -0,0 +1,14 @@
+import json
+lineups = {}
+with open('lineups.json', 'r') as file:
+    lineups = json.load(file)
+new_lineups = {}
+positions = ['RB1', 'RB2', 'WR1', 'WR2', 'Flex1', 'QB1', 'DE1', 'K1', 'TE1']
+for team in lineups.keys():
+    vals = lineups[team]
+    players = []
+    for position in positions:
+        players.append(vals[position].strip())
+    new_lineups[team] = players
+with open('serialized_lineups.json', 'w') as file:
+    file.write(json.dumps(new_lineups))
